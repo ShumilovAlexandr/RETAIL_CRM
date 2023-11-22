b35c548ee2075f2646ede94831033b2fc30f4b25
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2023-11-22 17:18:40 +0300

    Закончил с формированием массивов

--- src/services/service.py
+++ src/services/service.py
@@ -1,6 +1,4 @@
 import httpx
-import json
-import pandas as pd
 
 from fastapi import (APIRouter, 
                      Depends)
@@ -27,51 +25,72 @@ async def get_data_from_retailcrm():
     items = []
 
     try:
+        
         overall_result = httpx.get(f"{URL}?apiKey={API_KEY}").json()["orders"]
+        # for result in overall_result:
+        #     offer_ids = [res["offer"]["id"] for res in result["items"]]
+        #     for offer_id in offer_ids:
+        #         data_result = {
+        #             "offer_id": offer_id,
+        #             "bonusesCreditTotal": result["bonusesCreditTotal"],
+        #             "bonusesChargeTotal": result["bonusesChargeTotal"],
+        #             "externalId": result.get("externalId", []),
+        #             "orderType": result["orderType"],
+        #             "orderMethod": result.get("orderMethod", []),
+        #             "privilegeType": result["privilegeType"], 
+        #             "countryIso": result["countryIso"],
+        #             "createdAt": result["createdAt"],
+        #             "statusUpdatedAt": result["statusUpdatedAt"],
+        #             "summ": result["summ"],
+        #             "totalSumm": result["totalSumm"],
+        #             "prepaySum": result["prepaySum"],
+        #             "purchaseSumm": result["purchaseSumm"],
+        #             "markDatetime": result["markDatetime"],
+        #             "lastName": result.get("lastName", []),
+        #             "firstName": result["firstName"],
+        #             "phone": result.get("phone", []),
+        #             "email": result.get("email", []),
+        #             "call": result["call"],
+        #             "expired": result["expired"],
+        #             "site": result["site"],
+        #             "status": result["status"],
+        #             "fullPaidAt": result.get("fullPaidAt", []),
+        #             "fromApi": result["fromApi"],
+        #             "shipmentStore": result["shipmentStore"],
+        #             "shipped": result["shipped"],
+        #             "currency":  result["currency"]
+        #         }
+        #         data.append(data_result)
 
         for result in overall_result:
-
-            offer_ids = [res["offer"]["id"] for res in result["items"]]
-
-            for offer_id in offer_ids:
-
-                data_result = {
-                    "offer_id": offer_id,
-                    "bonusesCreditTotal": result["bonusesCreditTotal"],
-                    "bonusesChargeTotal": result["bonusesChargeTotal"],
-                    "externalId": result.get("externalId", []),
-                    "orderType": result["orderType"],
-                    "orderMethod": result.get("orderMethod", []),
-                    "privilegeType": result["privilegeType"], 
-                    "countryIso": result["countryIso"],
-                    "createdAt": result["createdAt"],
-                    "statusUpdatedAt": result["statusUpdatedAt"],
-                    "summ": result["summ"],
-                    "totalSumm": result["totalSumm"],
-                    "prepaySum": result["prepaySum"],
-                    "purchaseSumm": result["purchaseSumm"],
-                    "markDatetime": result["markDatetime"],
-                    "lastName": result.get("lastName", []),
-                    "firstName": result["firstName"],
-                    "phone": result.get("phone", []),
-                    "email": result.get("email", []),
-                    "call": result["call"],
-                    "expired": result["expired"],
-                    "site": result["site"],
-                    "status": result["status"],
-                    "fullPaidAt": result.get("fullPaidAt", []),
-                    "fromApi": result["fromApi"],
-                    "shipmentStore": result["shipmentStore"],
-                    "shipped": result["shipped"],
-                    "currency":  result["currency"]
-                }
-                data.append(data_result)
-
-        return data
-
-        
-
-
+            for res in result["items"]:
+                if res != []:
+                    items_result = {
+                        "bonusesChargeTotal": res["bonusesChargeTotal"],
+                        "bonusesCreditTotal": res["bonusesCreditTotal"],
+                        "initialPrice": res["initialPrice"],
+                        "discountTotal": res["discountTotal"],
+                        "vatRate": res["vatRate"],
+                        "createdAt": res["createdAt"],
+                        "quantity_1": res["quantity"],
+                        "status": res["status"],
+                        "purchasePrice": res["purchasePrice"],
+                        "ordering": res["ordering"],
+                        "offer_displayName": res["offer"]["displayName"],
+                        "offer_id": res["offer"]["id"],
+                        "offer_externalId": res["offer"]["externalId"],
+                        "offer_xmlId": res["offer"]["xmlId"],
+                        "offer_name": res["offer"]["name"],
+                        "offer_article": res["offer"]["article"],
+                        "offer_vatRate": res["offer"]["vatRate"],
+                        "offer_properties_type": res["offer"]["properties"]["type"],
+                        "offer_unit_code": res["offer"]["unit"]["code"],
+                        "offer_unit_name": res["offer"]["unit"]["name"],
+                        "offer_unit_sym": res["offer"]["unit"]["sym"],
+                        "price": int(str([price["price"] for price in res["prices"]])[1:-1])
+                    }
+                    items.append(items_result)
+        return items
         
     except httpx.HTTPError as exc:
         return {"message": f"HTTP Exception for {exc.request.url} - {exc}"}
